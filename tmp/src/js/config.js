21bec6326aada99a16e85a0d432d3ea865a813ac
Needle <77881374+imneedle@users.noreply.github.com>
2022-06-25 10:00:46 +0200

    update

--- tmp/src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '0498e425-fb12-4faa-95a7-29588b667b5b';
-export const MODAL_CLOSE_SEC = 2.5;
